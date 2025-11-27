6bd6cb1d948267fd2c2eef8be2b699f943660296
Great <sunnyagbo04@gmail.com>
2025-11-27 10:46:31 +0000

    add search contact feature

--- main.py
+++ main.py
@@ -17,4 +17,13 @@ add_contact("Alice", "1234567890")
 add_contact("Bob", "9876543210")
 
 # Viewing contacts
-view_contacts() 
+view_contacts()
+
+def search_contact(name):
+   found =[contact for contact in contacts if contact["name"].lower() == name.lower()]
+   if found:
+      for contact in found:
+          print(f"\nfound: {contact['name']} - {contact['phone']}")
+   else:
+      print("\nSorry, no contact found with the name you searched.")
+search_contact("Alice")
